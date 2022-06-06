32290cb1c834265f8d1c419dde0b5e14b0a6481d
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-06-06 12:37:08 +0000

    ajoue de MatToolbar modul et les code du headercomponent

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ] ,
+  exports:[
+    MatToolbarModule
+  ]
+})
+export class SharedModule { }
